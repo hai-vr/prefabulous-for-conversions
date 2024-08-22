a57326e7fde211d16b516980765a1a6391b12035
Haï~ <hai-vr@users.noreply.github.com>
2024-08-22 04:15:31 +0200

    Add Convert VRC Constraints to Unity Constraints: - Converts VRC Constraint components to native Unity Constraints, to the extent applicable. - VRC-specific features (local axis, freeze to world) are not supported. - The VRChat SDK assemblies and DLLs do not need to be installed in the project, as long as the types exist, so they could be mere stubs.

---
+++ Packages/dev.hai-vr.resilience.prefabulous.conversions/Scripts/Conversions/Components/PrefabulousConvertVRCConstraintsToUnityConstraints.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+#if PREFABULOUS_CONVERSIONS_VRCHAT_IS_INSTALLED
+using IPrefabulousEditorOnly = VRC.SDKBase.IEditorOnly;
+#else
+using Prefabulous.Universal.Shared.Runtime;
+#endif
+
+namespace Prefabulous.Conversions.Runtime
+{
+    [AddComponentMenu("Prefabulous/PA-Conversions VRC Constraints -> Unity Constraints")]
+    public class PrefabulousConvertVRCConstraintsToUnityConstraints : MonoBehaviour, IPrefabulousEditorOnly
+    {
+    }
+}
